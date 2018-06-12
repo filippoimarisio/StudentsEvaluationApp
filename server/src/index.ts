061bfa9746a076b456bb08ca0792121727ba98e4
filippoimarisio <filippo.imarisio@gmail.com>
2018-06-12 12:37:51 +0200

    app setup and batches list rendered

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,8 @@ import LoginController from './logins/controller';
 
 
 const app = createKoaServer({
-   controllers: [
+    cors: true,
+    controllers: [
      BatchController,
      StudentController,
      EvaluationController,
